85b3b1d55743ac219695fbd55952970fde1d7961
singhalanirudh18 <singhalanirudh18@gmail.com>
2017-05-28 14:06:33 +0530

    Add files via upload

---
+++ RTOS/RTOS_test_1/GccApplication9/main.c
@@ -0,0 +1,58 @@
+#ifndef F_CPU
+#define F_CPU 16000000UL // 16 MHz clock speed
+#endif
+
+#include <avr/io.h>
+#include <util/delay.h>
+//#include <windows.h>
+#include "osHeader.h"
+
+int main(void)
+{
+	DDRC = 0xFF; //Makes PORTC as Output
+	DDRA = 0xFF;
+	DDRB = 0xFF;
+	PORTC = 0xFF; 
+	createTask(Task1, 1, TaskId_1);
+	createTask(Task2, 2, TaskId_2);
+	startOS();
+	return 0;
+/*
+	while(1) //infinite loop
+	{
+		PORTC = 0xFF; //Turns ON All LEDs
+		_delay_ms(1000); //1 second delay
+		PORTC= 0x00; //Turns OFF All LEDs
+		_delay_ms(1000); //1 second delay
+	}
+*/	
+}
+void Task1()
+{
+
+	while(1)
+	{
+		PORTA = 0xFF; //Turns ON All LEDs
+		_delay_ms(1000); //1 second delay
+		PORTA= 0x00; //Turns OFF All LEDs
+		_delay_ms(1000); //1 second delay
+		//Stop the task
+	
+		waitTask();
+
+	}
+
+}
+
+void Task2()
+{
+	while(1)
+	{
+		PORTB = 0xFF; //Turns ON All LEDs
+		_delay_ms(1000); //1 second delay
+		PORTB= 0x00; //Turns OFF All LEDs
+		_delay_ms(1000); //1 second delay
+		//start the task
+		startTask(TaskId_1);
+	}
+}
